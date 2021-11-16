acc928a5a9bdbd62f419853d514da9fbb8738942
Andre Vaz <andre.vaz1411@gmail.com>
2021-11-16 16:52:00 +0000

    update dados

---
+++ dados.pl
@@ -0,0 +1,36 @@
+rua (Antonio_Sampaio, Gualtar, Braga).
+rua (Vasco_da_Gama, Gualtar, Braga).
+rua (Santa_Tuna_do_Assobio, Rio_Tinto, Porto).
+rua (Da_Torre, Bagunte, Porto).
+rua (Castelo_do_Norte, Nogueiro, Braga).
+rua (Afonso_de_Jeronimo, Prado, Braga).
+rua (Da_Fonte, Lumiar, Lisboa).
+rua (Da_Igreja, Lamacaes, Braga).
+rua (Familia_Real, Greenville, Algarve).
+rua (Torta, Esposende, Alentejo).
+
+cliente (Ogando).
+cliente (Paulo).
+cliente (Marlene).
+cliente (Susana).
+cliente (Jessica).
+cliente (Sandro).
+cliente (Leticia).
+cliente (Joao).
+cliente (Pedro).
+cliente (Manuel).
+
+estafeta (Roger).
+estafeta (Sheila).
+estafeta (Ilidio).
+estafeta (Carlos).
+estafeta (Catia).
+estafeta (Filipe).
+estafeta (Maria).
+estafeta (Oscar).
+estafeta (Abdul).
+estafeta (Aldrin).
+
+veiculo (bicicleta).
+veiculo (mota).
+veiculo (carro).
