8065e87e320a1402bc5fed8962927343504e92a1
Claudia54 <a93177@alunos.uminho.pt>
2022-01-07 04:05:50 +0000

    .

--- grafos.pl
+++ grafos.pl
@@ -3,12 +3,12 @@
 
 
 %Gerar os circuitos de entrega, caso existam, que cubram um determinado território
-% %não informada --> n sabe o q vem a seguir
+% ------------------------------------PESQUISA NÃO INFORMADA
 
 adjacente(X,Y,E) :- edge(X,Y,E).
 adjacente(X,Y,E) :- edge(Y,X,E).
 
-%BEST FIRST SEARCH
+%--------BEST FIRST SEARCH
 bfs(Orig, Dest, Cam):- bfs2(Dest,[[Orig]],Cam).
 bfs2(Dest,[[Dest|T]|_],Cam):- reverse([Dest|T],Cam). % Caminho aparece pela ordem inversa
 
@@ -19,7 +19,7 @@ bfs2(Dest,[LA|Outros],Cam) :- LA=[Act|_],
                               bfs2(Dest,Todos,Cam).
 
 
-%DEPTH FIRST SEARCH
+%---------DEPTH FIRST SEARCH
 dfs(Orig,Dest,Cam):- dfs2(Orig,Dest,[Orig],Cam). %condicao final: nó actual = destino
 dfs2(Dest,Dest,LA,Cam):- reverse(LA,Cam). %caminho actual esta invertido
 dfs2(Act,Dest,LA,Cam) :- adjacente(Act,X,_), %testar ligacao entre ponto actual e um qualquer X
@@ -32,7 +32,7 @@ listaDeAdjacentes(X, Lista):- findall(Y, adjacente(X, Y, _), Lista).
 
 
 
-%BUSCA ITERATIVA EM PROFUNDIDADE
+%----------BUSCA ITERATIVA EM PROFUNDIDADE
 
 depth_first_iterative_deepening(Node, Cam) :- 
                         path(Node, GoalNode, Solution),
@@ -46,8 +46,8 @@ path(FirstNode, LastNode, [LastNode|Path]) :-
                     adjacente(OneButLast, LastNode,_),
                     not(member(LastNode, Path)).
 
-
-%GULOSA
+%------------------------------------------PESQUISA INFORMADA 
+%----------GULOSA
 resolve_gulosa(Nodo, Caminho/Custo) :-
 	nodo(Nodo, Estima),
 	agulosa([[Nodo]/0/Estima], InvCaminho/Custo/_),
@@ -67,7 +67,7 @@ agulosa(Caminhos, SolucaoCaminho) :-
 
 obtem_melhor_g([Caminho], Caminho) :- !.
 
-obtem_melhor_g([Caminho1/Custo1/Est1,_/Custo2/Est2|Caminhos], MelhorCaminho) :-
+obtem_melhor_g([Caminho1/Custo1/Est1,_/_/Est2|Caminhos], MelhorCaminho) :-
 	Est1 =< Est2, !,
 	obtem_melhor_g([Caminho1/Custo1/Est1|Caminhos], MelhorCaminho).
 	
@@ -78,9 +78,7 @@ expande_gulosa(Caminho, ExpCaminhos) :-
 	findall(NovoCaminho, adjacente2(Caminho,NovoCaminho), ExpCaminhos).	
 
 
-
-%%ESTRELA
-
+%%-------------ESTRELA
 resolve_aestrela(Nodo, Caminho/Custo) :-
 	nodo(Nodo, Estima),
 	aestrela([[Nodo]/0/Estima], InvCaminho/Custo/_),
@@ -117,8 +115,14 @@ expande_aestrela(Caminho, ExpCaminhos) :-
 	findall(NovoCaminho, adjacente2(Caminho,NovoCaminho), ExpCaminhos).
 
 
-%CAMINHO MAIS RÁPIDO 
-%DFS
+
+
+
+
+
+
+%---------------------------------------------CAMINHO MAIS RÁPIDO 
+%---------DFS
 maisRapidoDFS(Inicio,Path, Cost):- maisRapidoDFS2(Inicio, [], 0, Path, Cost).
 
 maisRapidoDFS2(gualtar, Visited, Cost, Path, Cost):- reverse([gualtar|Visited], Path). 
@@ -128,59 +132,78 @@ maisRapidoDFS2(Node, Visited, Cost, Path, Total):- adjacente(Node, X, Value),
     maisRapidoDFS2(X, [Node|Visited], NewCost, Path, Total).
 
 solucao(Inicio,Lista) :- findall((Path,Cost),maisRapidoDFS(Inicio, Path, Cost),Lista).
-                                    %predsort(compare_by_second2,Lista,L),
-                                    %first(L,Res).
+                                   
 
-fim(Inicio, Path, Cost,Res):-solucao(Inicio,Lista), 
-                             predsort(compare_by_second2,Lista,L),
+fim(Inicio,Res):-solucao(Inicio,Path), 
+                             predsort(compare_by_second2,Path,L),
                              first(L,Res).
 
 
-first([H|T],R):- R=H.
+first([H|_],R):- R=H.
 
 
-%BFS
+%-------------BFS
+
 somasecond([],0).
-somasecond([[(U,H)|T]|I],S):-
-    somasecond(I,G),S is H+G.
+somasecond([[(_,H)|T]|_],S):-
+    somasecond(T,G),S is H+G.
 
-maisRapidoBFS(Orig, Dest, Cam,Custo):- maisRapidoBFS2(Dest,[[Orig]],Cam,0,Cost).
-maisRapidoBFS2(Dest,[[Dest|T]|_],Cam,Cost,Cost):- reverse([Dest|T],Cam). % Caminho aparece pela ordem inversa
+maisRapidoBFS(Orig, Dest, Cam,Custo):- maisRapidoBFS2(Dest,[[(Orig,0)]],Cam,0,Custo).
+maisRapidoBFS2(Dest,[[(Dest,Value)|T]|_],Cam,Cost,Cost):- reverse([(Dest,Value)|T],Cam). % Caminho aparece pela ordem inversa
 
-maisRapidoBFS2(Dest,[LA|Outros],Cam,Cost,Total):- LA=[Act|_],
+maisRapidoBFS2(Dest,[(X,Cost1)|Outros],Cam,Total):- LA=[(Act,Cost)|_],
                                                    findall([(X,Value)|LA],
-                                                   (Dest\==Act,adjacente(Act,X,Value),\+member(X,LA)),Novos),
+                                                   (Dest\==Act,adjacente(Act,X,Value),\+member((X,_),LA)),Novos),
                                                    somasecond(Novos,NewCost),
                                                    append(Outros,Novos,Todos),
                                                    maisRapidoBFS2(Dest,Todos,Cam,NewCost,Total).
 
 
+%---------------ESTRELA 
+
+maisRapido_aEstrela(Nodo,Maispequeno) :- findall(Caminho/Custo,resolve_aestrela(Nodo,Caminho/Custo),List),first(List,Maispequeno).
+
+%---------------GULOSA
+
+maisRapido_aGulosa(Nodo,Maispequeno) :- findall(Caminho/Custo,resolve_gulosa(Nodo,Caminho/Custo),List),first(List,Maispequeno).
 
 
-% MAIOR VOLUME E PESO DFS 
+
+
+
+
+
+% ----------------------------------------------MAIOR VOLUME E PESO DFS 
 encontraV(X,P1):-clienteerua(Cliente,rua(X,_)),caminhoV(Cliente,encomenda(_,P,V),_,_),P1 is P+V.
 
-%em algumas tem q se passar o seg elemento
+
 encontraPesoVNovo([H|T],P) :-encontraPesoV([H|T],0,P).
-%encontraPesoData(_,[],0).
-encontraPesoV([H|T],P,P).
+
+encontraPesoV(_,P,P).
 encontraPesoV([H|T],P,Total):-encontraV(H,P2),P1 is P+P2, encontraPesoV(T,P1,Total).
 
 auxencontra2([H|T],R):-findall((PesoVol,[H|T]),encontraPesoVNovo([H|T],PesoVol),List),reverse(List,PathR),first(PathR,R).
 
-
+%---------DFS
 volume_PesoDFS(Origem,Peso):-dfs(Origem,gualtar,Path),
                              auxencontra2(Path,Peso).
 
+
+%---------BFS
 volume_PesoBFS(Origem,Peso):-bfs(Origem,gualtar,Path),
                              auxencontra2(Path,Peso).
-
-volume_Peso_aStar(Origem,Peso):-resolve_aestrela(Origem,Path/Custo),
+%---------ESTRELA
+volume_Peso_aStar(Origem,Peso):-resolve_aestrela(Origem,Path/_),
                              auxencontra2(Path,Peso).
 
-
-volume_Peso_agulosa(Origem,Peso):-resolve_gulosa(Origem,Path/Custo),
+%--------GULOSA
+volume_Peso_agulosa(Origem,Peso):-resolve_gulosa(Origem,Path/_),
                                 auxencontra2(Path,Peso).
+
+
+
+
+
 
 
 %FUNÇÕES AUXILIARES PARA TEMPO
@@ -189,15 +212,15 @@ encontraVP(X,P,D):-clienteerua(Cliente,rua(X,_)),caminhoV(Cliente,encomenda(_,P,
 
 
 encontraPesoDataNovo([H|T],[(H,D)|F],P) :-encontraPesoData([H|T],[(H,D)|F],0,P).
-%encontraPesoData(_,[],0).
-encontraPesoData([H|T],[(H,D)|F],P,P).
+
+encontraPesoData([H|_],[(H,_)|_],P,P).
 encontraPesoData([H|T],[(H,D)|F],P,Total):-encontraVP(H,P2,D),P1 is P+P2, encontraPesoData(T,F,P1,Total).
 
 auxencontra([H|T],R):-findall((P,Path),encontraPesoDataNovo([H|T],Path,P),List),reverse(List,PathR),first(PathR,R).
 
 
 
-takefirst([H|T],T).
+takefirst([_|T],T).
 
 runLista([(R,T)|K],FinalLista):-percorreLista([(R,T)|K],[],FinalLista).
 
@@ -217,23 +240,25 @@ velocidadeVeiculo(PesoVol,carro,Velocidade):-Velocidade is 60-(0.1*PesoVol).
 getVelocidadeEncomenda(PV,List):- findall((Veiculo,Velocidade),(velocidadeVeiculo(PV,Veiculo,Velocidade)),List).
 
 comparaTempos([],_,_,_).
-comparaTempos([(X,Y)|T],Custo,Tempo,Veiculo,TempoTrans):- G is (Custo/Y),
+comparaTempos([(_,Y)|T],Custo,Tempo,Veiculo,TempoTrans):- G is (Custo/Y),
                                                           G >Tempo, comparaTempos(T,Custo,Tempo,Veiculo, TempoTrans).
-comparaTempos([(X,Y)|T],Custo,Tempo,Veiculo,TempoTrans) :- TempoTrans is (Custo/Y), TempoTrans =< Tempo, Veiculo = X, TempoTrans = TempoTrans.
+comparaTempos([(X,Y)|_],Custo,Tempo,Veiculo,TempoTrans) :- TempoTrans is (Custo/Y), TempoTrans =< Tempo, Veiculo = X, TempoTrans = TempoTrans.
 
 
 
 
 
-%TEMPO DFS
+%----------------------------------TEMPO, PRODUTIVIDADE E MELHOR VEICULO
+%DFS
 escolheVeiculoeTempoDFS(Origem,Path,Veiculo,Time,Custo):-maisRapidoDFS(Origem,Path,Custo),
                                     auxencontra(Path,(Peso,Caminho)), 
-                                    encontraMenorData(Caminho,(X,MaiorT)), 
+                                    encontraMenorData(Caminho,(_,MaiorT)), 
                                     getVelocidadeEncomenda(Peso,ListVelVeic),
                                     comparaTempos(ListVelVeic,Custo,MaiorT,Veiculo,Time).
 
 
-%TEMPO BFS 
+
+%BFS 
 
 /*escolheVeiculoeTempoBFS(Origem,Path,Veiculo,Time):-maisRapidoBFS(Origem,Path,Custo),
                                     auxencontra(Path,(Peso,Caminho)), 
@@ -245,7 +270,7 @@ escolheVeiculoeTempoDFS(Origem,Path,Veiculo,Time,Custo):-maisRapidoDFS(Origem,Pa
  %TEMPO GULOSA  
  escolheVeiculoeTempo_gulosa(Origem,Path,Veiculo,Time,Custo):-resolve_gulosa(Origem,Path/Custo),
                                                              auxencontra(Path,(Peso,Caminho)), 
-                                                             encontraMenorData(Caminho,(X,MaiorT)), 
+                                                             encontraMenorData(Caminho,(_,MaiorT)), 
                                                              getVelocidadeEncomenda(Peso,ListVelVeic),
                                                              comparaTempos(ListVelVeic,Custo,MaiorT,Veiculo,Time).
 
@@ -253,7 +278,7 @@ escolheVeiculoeTempoDFS(Origem,Path,Veiculo,Time,Custo):-maisRapidoDFS(Origem,Pa
 
 escolheVeiculoeTempo_estrela(Origem,Path,Veiculo,Time,Custo):-resolve_aestrela(Origem,Path/Custo),
                                                             auxencontra(Path,(Peso,Caminho)), 
-                                                            encontraMenorData(Caminho,(X,MaiorT)), 
+                                                            encontraMenorData(Caminho,(_,MaiorT)), 
                                                             getVelocidadeEncomenda(Peso,ListVelVeic),
                                                             comparaTempos(ListVelVeic,Custo,MaiorT,Veiculo,Time).
                             
